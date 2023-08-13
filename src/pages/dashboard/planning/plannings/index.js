38b2280cead2ea8a7216c1da85e3fcbbb4716b96
MuhammadMujtabaa <51963629+MuhammadMujtabaa@users.noreply.github.com>
2023-08-13 23:04:21 +0500

    updated

--- src/pages/dashboard/planning/plannings/index.js
+++ src/pages/dashboard/planning/plannings/index.js
@@ -1,17 +1,18 @@
-import React, { Fragment, useRef } from "react";
+import moment from "moment";
+import React, { Fragment, useEffect, useRef, useState } from "react";
 import { Button, Col, Container, Row } from "react-bootstrap";
 import Dropdown from "react-bootstrap/Dropdown";
-import Select from "react-select";
-import moment from "moment";
-import { useEffect, useState } from "react";
-import { useLocation, useNavigate } from "react-router-dom";
 import { BsCalendar3, BsTelephone } from "react-icons/bs";
-import { MdArrowForwardIos, MdArrowBackIosNew } from "react-icons/md";
+import { MdArrowBackIosNew, MdArrowForwardIos } from "react-icons/md";
+import { SlArrowDown, SlArrowUp } from "react-icons/sl";
+import { useLocation, useNavigate } from "react-router-dom";
+import Select from "react-select";
+
 import { AiOutlinePlus } from "react-icons/ai";
 import PageLayout from "../../../../layouts/PageLayout";
 
-import "../planning.css";
 import Flatpickr from "react-flatpickr";
+import "../planning.css";
 const shiftStatus = [
   {
     label: "Fulfilled",
@@ -50,6 +51,11 @@ export default function Planning() {
   const [selectedDate, setSelectedDate] = useState(
     moment().format("yyyy-MM-DD")
   );
+  const [toggle, setToggle] = useState({
+    toggle1: false,
+    toggle2: false,
+    toggle3: false,
+  });
 
   useEffect(() => {
     setSelectedDate(query.get("on"));
@@ -82,10 +88,11 @@ export default function Planning() {
   };
 
   const handleClickDatePicker = (date) => {
-    navigate(
-        `/planning?on=${moment(date[0]).format("yyyy-MM-DD")}`
-      )
- 
+    navigate(`/planning?on=${moment(date[0]).format("yyyy-MM-DD")}`);
+  };
+
+  const handleToggle = (toggleName) => {
+    setToggle((prev) => ({ ...prev, [toggleName]: !prev?.[toggleName] }));
   };
   return (
     <PageLayout>
@@ -135,7 +142,6 @@ export default function Planning() {
                             datePickerRef.current.flatpickr.open();
                           }}
                         >
-                          <div></div>
                           <BsCalendar3 />
                         </Button>
                         <Button
@@ -146,7 +152,9 @@ export default function Planning() {
                           <MdArrowForwardIos />
                         </Button>
                       </div>
-                      <div  style={{ width: 0, height: 0, padding: 0, border: 0 }}>
+                      <div
+                        style={{ width: 0, height: 0, padding: 0, border: 0 }}
+                      >
                         <Flatpickr
                           ref={datePickerRef}
                           style={{ width: 0, height: 0, padding: 0, border: 0 }}
@@ -272,66 +280,102 @@ export default function Planning() {
                   <Row>
                     <Col lg={12} className="px-0">
                       <div
-                        className="d-flex w-100 align-items-center px-2 cursor-pointer"
+                        className="d-flex w-100 justify-content-between align-items-center px-2 cursor-pointer"
                         style={{ height: "50px" }}
+                        onClick={() => handleToggle("toggle1")}
                       >
-                        <span className="fw-bold">Liebr Holding B.V.</span>
+                        <div>
+                          <span className="fw-bold">Liebr Holding B.V.</span>
+                        </div>
+                        <div className="px-2">
+                          {toggle?.toggle1 ? <SlArrowUp /> : <SlArrowDown />}
+                        </div>
                       </div>
                     </Col>
                   </Row>
-                  <Row>
-                    <Col lg={12} className="px-0">
-                      <div
-                        className="d-flex w-100 align-items-center border border-bottom-0 px-2 cursor-pointer"
-                        style={{ height: "50px" }}
-                      >
-                        <span className="fw-bold">Algemeen</span>
-                      </div>
-                    </Col>
-                  </Row>
-                  <Row>
-                    <Col lg={3} className="px-0">
-                      <div
-                        className="d-flex w-100 align-items-center border border-end-0 px-2"
-                        style={{ height: "70px" }}
-                      >
-                        <span>Schoonmaker</span>
-                      </div>
-                    </Col>
-                    <Col lg={9} className="px-0 d-flex">
-                      {Array.from({ length: selectedDayOption?.value })?.map(
-                        (item, index) => {
-                          const dateAndDay = moment(selectedDate).add(
-                            index,
-                            "day"
-                          );
-                          return (
-                            <div
-                              className="d-flex align-items-center border-top border-bottom"
-                              style={{
-                                height: "70px",
-                                background:
-                                  index % 2 == 0 ? "#F3F2F3" : "#FBFBFB",
-                                width: `${100 / selectedDayOption?.value}%`,
-                                cursor: "pointer",
-                              }}
-                            >
-                              <div
-                                className="w-100 h-100 d-flex justify-content-center align-items-center planning-plus-button"
-                                onClick={() =>
-                                  handleClickCreateShift(dateAndDay)
-                                }
-                              >
-                                <span>
-                                  <AiOutlinePlus size={25} />
-                                </span>
-                              </div>
+                  {toggle?.toggle1 ? (
+                    <>
+                      <Row>
+                        <Col lg={12} className="px-0">
+                          <div
+                            className={`d-flex w-100 justify-content-between align-items-center border px-2 cursor-pointer ${
+                              toggle?.toggle2 ? "border-bottom-0" : ""
+                            }`}
+                            style={{ height: "50px" }}
+                            onClick={() => handleToggle("toggle2")}
+                          >
+                            <div>
+                              <span className="fw-bold">Algemeen</span>
                             </div>
-                          );
-                        }
-                      )}
-                    </Col>
-                  </Row>
+                            <div className="px-2">
+                              {toggle?.toggle2 ? (
+                                <SlArrowUp />
+                              ) : (
+                                <SlArrowDown />
+                              )}
+                            </div>
+                          </div>
+                        </Col>
+                      </Row>
+                      {toggle?.toggle2 ? (
+                        <>
+                          <Row>
+                            <Col lg={3} className="px-0">
+                              <div
+                                className="d-flex w-100 align-items-center border border-end-0 px-2"
+                                style={{ height: "70px" }}
+                              >
+                                <span>Schoonmaker</span>
+                              </div>
+                            </Col>
+                            <Col lg={9} className="px-0 d-flex">
+                              {Array.from({
+                                length: selectedDayOption?.value,
+                              })?.map((item, index) => {
+                                const dateAndDay = moment(selectedDate).add(
+                                  index,
+                                  "day"
+                                );
+                                return (
+                                  <div
+                                    className="d-flex align-items-center border-top border-bottom"
+                                    style={{
+                                      height: "70px",
+                                      background:
+                                        index % 2 == 0 ? "#F3F2F3" : "#FBFBFB",
+                                      width: `${
+                                        100 / selectedDayOption?.value
+                                      }%`,
+                                      cursor: "pointer",
+                                    }}
+                                  >
+                                    <div
+                                      className="w-100 h-100 d-flex justify-content-center align-items-center planning-plus-button"
+                                      onClick={() =>
+                                        handleClickCreateShift(dateAndDay)
+                                      }
+                                    >
+                                      <span>
+                                        <AiOutlinePlus size={25} />
+                                      </span>
+                                    </div>
+                                  </div>
+                                );
+                              })}
+                            </Col>
+                          </Row>
+                        </>
+                      ) : null}
+                    </>
+                  ) : (
+                    <>
+                      <Row>
+                        <Col lg={12} className="px-0">
+                          <div className="d-flex w-100 align-items-center border border-bottom-0 px-2 cursor-pointer"></div>
+                        </Col>
+                      </Row>
+                    </>
+                  )}
                 </Col>
               </Row>
             </Col>
