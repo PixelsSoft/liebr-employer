df92ae1e2de66c210d238ebcdccdff7565eab06f
MuhammadMujtabaa <51963629+MuhammadMujtabaa@users.noreply.github.com>
2023-08-13 15:24:25 +0500

    create planning screen updated

--- src/pages/dashboard/planning/createPlanning/index.js
+++ src/pages/dashboard/planning/createPlanning/index.js
@@ -1,6 +1,12 @@
 import React, { Fragment } from "react";
 import { Button, Card, Col, Container, Row } from "react-bootstrap";
 import Select from "react-select";
+import InputNumber from "rc-input-number";
+import Tab from "react-bootstrap/Tab";
+import Tabs from "react-bootstrap/Tabs";
+import Form from "react-bootstrap/Form";
+import Table from "react-bootstrap/Table";
+
 const CreatePlannings = () => {
   return (
     <Fragment>
@@ -40,7 +46,7 @@ const CreatePlannings = () => {
           </Col>
           <Col></Col>
         </Row>
-        <Row className="bg-light text-dark py-1 min-vh-100">
+        <Row className="bg-light text-dark py-1">
           <Col></Col>
           <Col lg={10}>
             <Row>
@@ -48,37 +54,244 @@ const CreatePlannings = () => {
                 <p className="py-3">
                   <span className="fw-bold">Create a shift</span>
                 </p>
-                <p className="mb-0">
-                  <small className="">Date and time</small>
-                </p>
-                <Row>
-                  <Col lg={3}></Col>
-                  <Col lg={3}>
-                    <Card className="border-0 shadow-sm rounded-0 p-2">
-                      <Card.Body>
-                        <div>
-                          <div>
-                            <Card.Text className="fw-normal text-center">
-                              Similar shifts in your region are on average
-                              €20.90 per hour (including usage fee)
-                            </Card.Text>
-                          </div>
-                          <div className="mt-3 d-flex justify-content-center">
-                            <Button className="px-3 py-2 rounded-1">
-                              <span>I want the same for this shift</span>
-                            </Button>
-                          </div>
-                        </div>
-                      </Card.Body>
-                    </Card>
-                  </Col>
-                  <Col lg={6}></Col>
-                </Row>
+                <div className="d-flex flex-column gap-3">
+                  <div>
+                    <p className="mb-0">
+                      <small className="">Date and time</small>
+                    </p>
+                    <Row>
+                      <Col lg={3}></Col>
+                      <Col lg={3}>
+                        <Card className="border-0 shadow-sm rounded-0 p-2">
+                          <Card.Body>
+                            <div>
+                              <div>
+                                <Card.Text className="fw-normal text-center">
+                                  Similar shifts in your region are on average
+                                  €20.90 per hour (including usage fee)
+                                </Card.Text>
+                              </div>
+                              <div className="mt-3 d-flex justify-content-center">
+                                <Button className="px-3 py-2 rounded-1">
+                                  <span>I want the same for this shift</span>
+                                </Button>
+                              </div>
+                            </div>
+                          </Card.Body>
+                        </Card>
+                      </Col>
+                      <Col lg={6}></Col>
+                    </Row>
+                  </div>
+                  <div>
+                    <p className="mb-0">
+                      <small className="">Number of FreeFlexers</small>
+                    </p>
+                    <Row>
+                      <Col lg={3}>
+                        <InputNumber
+                          aria-label="Simple number input example"
+                          min={-8}
+                          max={10}
+                        />
+                      </Col>
+                      <Col lg={3}></Col>
+                      <Col lg={6}></Col>
+                    </Row>
+                  </div>
+                  <div>
+                    <p className="mb-0">
+                      <small className="">Total cost per hour</small>
+                    </p>
+                    <Row>
+                      <Col lg={6}>
+                        <Select options={[]} />
+                      </Col>
+
+                      <Col lg={6}></Col>
+                    </Row>
+                  </div>
+                </div>
               </Col>
             </Row>
           </Col>
           <Col></Col>
         </Row>
+        <Row className="bg-light text-dark py-1 py-5">
+          <Col></Col>
+          <Col lg={10}>
+            <Row>
+              <Col lg={12}>
+                <Tabs
+                  defaultActiveKey="profile"
+                  id="uncontrolled-tab-example"
+                  className="mb-3"
+                >
+                  <Tab eventKey="details" title="Details">
+                    <div className="d-flex flex-column gap-3">
+                      <div>
+                        <p className="mb-0">
+                          <small className="">
+                            Automatically accept FreeFlexers from your Flexpool
+                          </small>
+                        </p>
+                        <Row>
+                          <Col lg={5}>
+                            <div className="d-flex flex-column gap-3">
+                              <div>
+                                <Select options={[]} />
+                              </div>
+                              <div>
+                                <Card className="border-0 shadow-sm rounded-0">
+                                  <Card.Body>
+                                    <div className="d-flex justify-content-between">
+                                      <div>
+                                        <p className="fw-normal">
+                                          48 hours before the start of the
+                                          shift, auto-accept FreeFlexers that
+                                          have frequently worked with me.
+                                        </p>
+                                      </div>
+                                      <div>
+                                        <Form>
+                                          <Form.Check type="switch" />
+                                        </Form>
+                                      </div>
+                                    </div>
+                                  </Card.Body>
+                                </Card>
+                              </div>
+                            </div>
+                          </Col>
+
+                          <Col></Col>
+                        </Row>
+                      </div>
+                      <div>
+                        <p className="mb-0">
+                          <small className="">Cancellation policy</small>
+                        </p>
+                        <Row>
+                          <Col lg={5}>
+                            <Select options={[]} />
+                          </Col>
+
+                          <Col></Col>
+                        </Row>
+                      </div>
+                    </div>
+                  </Tab>
+                  <Tab eventKey="shift Roles" title="Shift Roles">
+                    <div className="d-flex flex-column gap-3">
+                      <div>
+                        <p className="mb-0">
+                          <span className="">
+                            Select who can manage this shift
+                          </span>
+                        </p>
+                        <Row>
+                          <Col lg={3}>
+                            <div className="d-flex flex-column gap-3 w-100">
+                              <div>
+                                <Button
+                                  variant="white"
+                                  className="px-4 py-2 rounded-1 shadow-sm border w-100 bg-white text-start"
+                                >
+                                  <span>Add User</span>
+                                </Button>
+                              </div>
+                            </div>
+                          </Col>
+                          <Col></Col>
+                        </Row>
+                      </div>
+                      <div>
+                        <Row>
+                          <Col lg={8}>
+                            <Table width={"100%"}>
+                              <thead>
+                                <tr>
+                                  <th
+                                    className="bg-transparent"
+                                    width="60%"
+                                  ></th>
+                                  <th className="bg-transparent" width="20%">
+                                    <small className="fw-normal">
+                                      Edit / Cancel Shift
+                                    </small>
+                                  </th>
+                                  <th className="bg-transparent" width="20%">
+                                    <small className="fw-normal">
+                                      Select Applicants
+                                    </small>
+                                  </th>
+                                  <th className="bg-transparent" width="20%">
+                                    <small className="fw-normal">
+                                      Checkout
+                                    </small>
+                                  </th>
+                                </tr>
+                              </thead>
+                              <tbody className="shadow-sm">
+                                {Array.from({ length: 4 })?.map((item) => {
+                                  return (
+                                    <tr>
+                                      <td width="60%">
+                                        <div className="d-flex">
+                                          <span className="fw-bold">El Essakkati</span>
+                                        </div>
+                                      </td>
+                                      <td width="20%">
+                                        <Form>
+                                          <Form.Check type="switch" />
+                                        </Form>
+                                      </td>
+                                      <td width="20%">
+                                        <Form>
+                                          <Form.Check type="switch" />
+                                        </Form>
+                                      </td>
+                                      <td width="20%">
+                                        <Form>
+                                          <Form.Check type="switch" />
+                                        </Form>
+                                      </td>
+                                    </tr>
+                                  );
+                                })}
+                                {/* <tr>
+                                  <td>1</td>
+                                  <td>Mark</td>
+                                  <td>Otto</td>
+                                  <td>@mdo</td>
+                                </tr>
+                                <tr>
+                                  <td>2</td>
+                                  <td>Jacob</td>
+                                  <td>Thornton</td>
+                                  <td>@fat</td>
+                                </tr>
+                                <tr>
+                                  <td>3</td>
+                                  <td colSpan={2}>Larry the Bird</td>
+                                  <td>@twitter</td>
+                                </tr> */}
+                              </tbody>
+                            </Table>
+                          </Col>
+
+                          <Col></Col>
+                        </Row>
+                      </div>
+                    </div>
+                  </Tab>
+                </Tabs>
+              </Col>
+            </Row>
+          </Col>
+          <Col></Col>
+        </Row>
+
         <Row className="bg-white shadow py-4 position-sticky sticky-bottom ">
           <Col></Col>
           <Col lg={10}>
