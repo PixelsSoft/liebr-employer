5bb5dc54e37d81ff793ef9959ee4926b41322310
MuhammadMujtabaa <51963629+MuhammadMujtabaa@users.noreply.github.com>
2023-08-16 01:10:38 +0500

    new labels are added

--- src/assets/labels/dutch.js
+++ src/assets/labels/dutch.js
@@ -25,7 +25,17 @@ const labels = {
   flexibleStartAndEndTime: "Flexible start and end time",
   yourCurrentCreditIsTooLowToAllowFactoringForFreeFlexersThisSignificantlyReducesTheChanceOfFindingSuitableFreeflexersWeRecommendThatYouIncreaseYourCreditBeforeYouPlaceTheShift:
     "Your current credit is too low to allow factoring for FreeFlexers, this significantly reduces the chance of finding suitable FreeFlexersWe recommend that you increase your credit before you place the shift.",
-    numberOfFreeflexers:"Number of FreeFlexers",
-    totalCostPerHour:"Total cost per hour",
-    automaticallyAcceptFreeflexersFromYourFlexpool:"Automatically accept FreeFlexers from your Flexpool"
+  numberOfFreeflexers: "Number of FreeFlexers",
+  totalCostPerHour: "Total cost per hour",
+  automaticallyAcceptFreeflexersFromYourFlexpool:
+    "Automatically accept FreeFlexers from your Flexpool",
+  hoursBeforeTheStartOfTheShiftAutoAcceptFreeflexersThatHaveFrequentlyWorkedWithMe:
+    "48 hours before the start of the shift, auto-accept FreeFlexers that have frequently worked with me.",
+  cancellationPolicy: "Cancellation policy",
+  selectWhoCanManageThisShift: "Select who can manage this shift",
+  editCancelShift: "Edit / Cancel Shift",
+  selectApplicants: "Select Applicants",
+  checkout: "Checkout",
+  yourCurrentCreditIsTooLowToAllowFactoringForFreeflexersThisSignificantlyReducesTheChanceOfFindingSuitableFreeflexersWeRecommendThatYouIncreaseYourCreditBeforeYouPlaceTheShift: `Your current credit is too low to allow factoring for FreeFlexers, this significantly reduces the chance of finding suitable FreeFlexers. We recommend that you increase your credit before you place the shift.`,
+  placeAndAddAnother:"Place and add another"
 };
