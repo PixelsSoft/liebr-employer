a67f427be12b296530c04cd2dd9f90a9d0aa14e0
MuhammadMujtabaa <51963629+MuhammadMujtabaa@users.noreply.github.com>
2023-08-16 00:58:37 +0500

    new labels

---
+++ src/assets/labels/dutch.js
@@ -0,0 +1,31 @@
+const labels = {
+  logInWithEmail: "Log in with email",
+  email: "email",
+  password: "password",
+  forgotPasswordYouCanEitherChooseTo:
+    "Forgot password? You can either choose to",
+  loginWithoutAPassword: "Login without a password",
+  login: "login",
+  loginWithoutAPassword: "Login without a password",
+  pleaseEnterYourEmailAddressBelowAndWellSendYouALinkToLogin:
+    "Please enter your email address below, and we'll send you a link to login.",
+  loginWithoutAPassword: "Login without a password",
+  resetPassword: "Reset password",
+  sendLink: "Send link",
+  week: "week",
+  needHelpContactOurSupportDepartment: "NeedHelp?ContactOurSupportDepartment",
+  days: "days",
+  shiftStatuses: "Shift Statuses",
+  dateAndTime: "Date and time",
+  similarShiftsInYourRegionAreOnAveragePerHourIncludingUsageFee:
+    "Similar shifts in your region are on average €20.90 per hour (including usage fee)",
+  start: "Start",
+  dnd: "End",
+  createAShift: "Create a shift",
+  flexibleStartAndEndTime: "Flexible start and end time",
+  yourCurrentCreditIsTooLowToAllowFactoringForFreeFlexersThisSignificantlyReducesTheChanceOfFindingSuitableFreeflexersWeRecommendThatYouIncreaseYourCreditBeforeYouPlaceTheShift:
+    "Your current credit is too low to allow factoring for FreeFlexers, this significantly reduces the chance of finding suitable FreeFlexersWe recommend that you increase your credit before you place the shift.",
+    numberOfFreeflexers:"Number of FreeFlexers",
+    totalCostPerHour:"Total cost per hour",
+    automaticallyAcceptFreeflexersFromYourFlexpool:"Automatically accept FreeFlexers from your Flexpool"
+};
